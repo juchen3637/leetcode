99bc9f7216c154030d4f2b9c8995ff2948482951
juchen3637 <103432641+juchen3637@users.noreply.github.com>
2022-09-26 00:21:16 -0400

    stuff

--- invertBinaryTree/invertBinaryTree.cpp
+++ invertBinaryTree/invertBinaryTree.cpp
@@ -13,16 +13,17 @@ public:
     if (root == nullptr) { // if root is at null then return the previous root, basically goes back up a node in the binary tree
         return root;
     }
-    invertTree(root->left);
-    invertTree(root->right);
 
-    TreeNode *left = root->left;
-    TreeNode *right = root->right;
+    TreeNode *left = root->left; // saves the left node from root
+    TreeNode *right = root->right; // saves the right node from root
 
-    root->left = right;
-    root->right = left;
+    invertTree(root->left); // calls the function again, setting root equal to its left node, going down the binary tree
+    invertTree(root->right); // calls the function again, setting root equal to its right node, going down the binary tree
 
-    return root;
+    root->left = right; // swaps left node with right node
+    root->right = left; // swaps right node with left node
+
+    return root; // returns the binary tree
 
     }
 };
