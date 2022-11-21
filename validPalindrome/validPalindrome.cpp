069e3a2c03fb4d076ab089865ab52b1502b9e825
juchen3637 <103432641+juchen3637@users.noreply.github.com>
2022-11-20 21:53:16 -0500

    fix palindrome code

--- validPalindrome/validPalindrome.cpp
+++ validPalindrome/validPalindrome.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
+#include <bits/stdc++.h> 
 using namespace std;
 
 class Solution {
 public:
     bool isPalindrome(string s) {
-      const char* initialChar = s.c_str();
-      
+      char initialChar[s.length() + 1];
+      strcpy(initialChar, s.c_str());
+      int sizeOfCharArray = sizeof(initialChar) / sizeof(initialChar[0]);
+      for (int i = 0; i < sizeOfCharArray/2; i++) {
+        for (int j = sizeOfCharArray - 1; j > sizeOfCharArray / 2; j--) {
+          if (initialChar[i] == initialChar[j]) {
+          }
+          else {
+            return false;
+          }
+        } 
+      }
+
+      return true;
     }
 };
