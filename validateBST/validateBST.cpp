b29ad9e23f8895d407894f242947348d6c7528f9
juchen3637 <103432641+juchen3637@users.noreply.github.com>
2023-01-29 17:44:36 -0500

    Update validateBST.cpp

--- validateBST/validateBST.cpp
+++ validateBST/validateBST.cpp
@@ -12,21 +12,17 @@ struct TreeNode {
 };
 
 class Solution {
- public:
-  bool isValidBST(TreeNode *root) { 
-    int key = root->val;
-    if (root->left->val < key) {
-      root = root->left;
-      isValidBST;
-    }
-
-    else if (root->right->val > key) {
-      root = root->right;
-      isValidBST;
-    }
-
-    else {
-      return false;
-    }
+ public:// members can be accessed outside of class
+  bool isValidBST(TreeNode *root) { return helper(root, LONG_MIN, LONG_MAX); //uses LONG_MIN as neg infinity and LONG_MAX as positive infinity parameters
    }
+
+ private: //members cannot be accessed outside of class
+  bool helper(TreeNode *root, long left, long right) {
+    if (!root) return true; // if root == 0 then return true, i.e. if the tree checks everything 
+    if (root->val < right && root->val > left) { // checks if the value of right is greater than root and value of left is less than root
+      return helper(root->left, left, root->val) && // recursion, passes the left value of root back into the function and checks its childern for the same thing as the above statement
+             helper(root->right, root->val, right); // recursion, this passes the right value of root back into the function and checks its childern
+    }
+    return false; //if the values of the BST are, left greater than root and right less than root it is not a valid BST, return false
+  }
 };
